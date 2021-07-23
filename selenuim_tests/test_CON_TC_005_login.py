3363e0d2f01c3be1103424933b533c2cd65440d6
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-07-23 15:29:01 +0200

    Fixes

--- selenuim_tests/test_CON_TC_005_login.py
+++ selenuim_tests/test_CON_TC_005_login.py
@@ -12,20 +12,20 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), optio
 # Conduit megnyitása
 driver.get("http://localhost:1667")
 driver.maximize_window()
-time.sleep(2)
+driver.implicitly_wait(10)
 
 
 def test_login():
     # Sign in gombra kattintás
     driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     # Bejelentkezési adatok kitöltése
     driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input').send_keys(
         'testuser1@example.com')
     driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input').send_keys('Abcd123$')
     driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     assert (driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[4]/a').text == 'testuser1')
 

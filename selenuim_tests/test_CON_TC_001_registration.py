9bfc1cfddfcbf7d5162ef64ebfa860fe211977a1
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-07-23 14:24:20 +0200

    Adding selenium test-cases.

--- selenuim_tests/test_CON_TC_001_registration.py
+++ selenuim_tests/test_CON_TC_001_registration.py
@@ -24,28 +24,31 @@ def test_reg():
 
     # Conduit megnyitása
     driver.get("http://localhost:1667")
-    time.sleep(2)
+    driver.implicitly_wait(10)
+
     # Sign up gombra kattintás
     driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[3]/a').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
+
     # Kitöltendő mezők kinyerése
     username = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
     email = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
     password = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[3]/input')
     sign_up_button = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
-    time.sleep(2)
+    driver.implicitly_wait(10)
+
     # Mezők kitöltése tesztadatokkal
     username.send_keys(random_user)
     email.send_keys(random_user + '@example.com')
     password.send_keys('Abcd123$')
     sign_up_button.click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     # A regisztráció sikerességének ellenőrzése
     assert (driver.find_element_by_xpath('/html/body/div[2]/div/div[3]').text == 'Your registration was successful!')
 
     driver.find_element_by_xpath('/html/body/div[2]/div/div[4]/div/button').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     # Bejelentkezés után a felhasználónév ellenőrzése
     assert (driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[4]/a').text == random_user)
--- selenuim_tests/test_CON_TC_005_login.py
+++ selenuim_tests/test_CON_TC_005_login.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+# import time
 
 opt = Options()
 opt.headless = True
@@ -12,16 +12,18 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), optio
 def test_login():
     # Conduit megnyitása
     driver.get("http://localhost:1667")
-    time.sleep(2)
+    driver.implicitly_wait(10)
+
     # Sign in gombra kattintás
     driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
+
     # Bejelentkezési adatok kitöltése
     driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input').send_keys(
         'testuser1@example.com')
     driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input').send_keys('Abcd123$')
     driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     assert (driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[4]/a').text == 'testuser1')
 

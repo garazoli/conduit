6ebc4e3c8762db4654c78788c8ed7247d67dd62c
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-07-24 11:01:40 +0200

    New testcases + Fixes

--- selenuim_tests/test_A003_reg_invalid_pwd.py
+++ selenuim_tests/test_A003_reg_invalid_pwd.py
@@ -15,7 +15,7 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), optio
 
 # Conduit megnyitása
 driver.get("http://localhost:1667")
-driver.implicitly_wait(2)
+driver.implicitly_wait(3)
 
 
 def test_reg_pwd():
@@ -31,21 +31,21 @@ def test_reg_pwd():
 
     # Sign up gombra kattintás
     driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[3]/a').click()
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(3)
 
     # Kitöltendő mezők kinyerése
     username = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
     email = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
     password = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[3]/input')
     sign_up_button = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(3)
 
     # Mezők kitöltése tesztadatokkal
     username.send_keys(random_user)
     email.send_keys(random_user + '@example.com')
     password.send_keys('1234')
     sign_up_button.click()
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(3)
 
     # A regisztráció sikerességének ellenőrzése
     assert (driver.find_element_by_xpath(

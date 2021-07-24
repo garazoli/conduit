4308f8b81dceb985bed2c487075946e7af7ed5ff
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-07-24 10:46:17 +0200

    New testcases + Fixes

--- selenuim_tests/test_A003_reg_invalid_pwd.py
+++ selenuim_tests/test_A003_reg_invalid_pwd.py
@@ -1,7 +1,8 @@
-# TC_002 teszteset
+# TC_003 teszteset
 
 import random
 import string
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -15,10 +16,10 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), optio
 # Conduit megnyitása
 driver.get("http://localhost:1667")
 # driver.maximize_window()
-driver.implicitly_wait(10)
+driver.implicitly_wait(2)
 
 
-def test_reg_email():
+def test_reg_pwd():
     # Random username és email generálás
     class MyRND:
         chars = string.ascii_lowercase
@@ -31,23 +32,25 @@ def test_reg_email():
 
     # Sign up gombra kattintás
     driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[3]/a').click()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(2)
 
     # Kitöltendő mezők kinyerése
     username = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
     email = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
     password = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[3]/input')
     sign_up_button = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(2)
 
     # Mezők kitöltése tesztadatokkal
     username.send_keys(random_user)
-    email.send_keys(random_user + 'vexample.com')
-    password.send_keys('Abcd123$')
+    email.send_keys(random_user + '@example.com')
+    password.send_keys('1234')
     sign_up_button.click()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(2)
 
     # A regisztráció sikerességének ellenőrzése
-    assert (driver.find_element_by_xpath('/html/body/div[2]/div/div[3]').text == 'Email must be a valid email.')
+    assert (driver.find_element_by_xpath(
+        '/html/body/div[2]/div/div[3]').text == 'Password must be 8 characters long and include 1 number, 1 uppercase '
+                                                'letter, and 1 lowercase letter.')
 
     driver.close()

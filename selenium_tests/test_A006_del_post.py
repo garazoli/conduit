8825fbfc1beb1dcc900395a64a4da524eb847eb6
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-08-03 19:45:07 +0200

    Fixes.

--- selenium_tests/test_A006_del_post.py
+++ selenium_tests/test_A006_del_post.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import locators
-
+import time
 # Headless mode
 opt = Options()
 opt.headless = True
@@ -19,26 +19,26 @@ driver.implicitly_wait(10)
 def test_del_post():
     # Sign in gombra kattintás
     driver.find_element_by_xpath(locators.sign_in_x).click()
-    driver.implicitly_wait(10)
+    time.sleep(20)
 
     # Bejelentkezési adatok kitöltése
     driver.find_element_by_xpath(locators.si_email_x).send_keys('testuser1@example.com')
     driver.find_element_by_xpath(locators.si_password_x).send_keys('Abcd123$')
     driver.find_element_by_xpath(locators.sign_in_button_x).click()
-    driver.implicitly_wait(10)
+    time.sleep(20)
 
     # Törölni kívánt bejegyzés kiválasztása és törlése
     driver.find_element_by_xpath(locators.user_x).click()
-    driver.implicitly_wait(10)
+    time.sleep(20)
     title_post = driver.find_element_by_xpath(locators.first_post_x)  # Az első elem címe
     title_post.click()
-    driver.implicitly_wait(30)
+    time.sleep(20)
     driver.find_element_by_xpath(locators.delete_button_x).click()
-    driver.implicitly_wait(10)
+    time.sleep(20)
 
     # Elem törlésének ellenőrzése:
     driver.find_element_by_xpath(locators.user_x).click()  # Visszanavigálás a saját postok közé
-    driver.implicitly_wait(10)
+    time.sleep(20)
     title_next_post = driver.find_element_by_xpath(locators.first_post_x)  # Az első elem címe a törlés után
 
     assert (title_post != title_next_post)
--- selenium_tests/test_A007_edit_post.py
+++ selenium_tests/test_A007_edit_post.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import locators
+import time
 
 # Headless mode
 opt = Options()
@@ -20,13 +21,13 @@ def test_edit_post():
     # Bejelentkezés
     # Sign in gombra kattintás
     driver.find_element_by_xpath(locators.sign_in_x).click()
-    driver.implicitly_wait(10)
+    time.sleep(20)
 
     # Bejelentkezési adatok kitöltése
     driver.find_element_by_xpath(locators.si_email_x).send_keys('testuser1@example.com')
     driver.find_element_by_xpath(locators.si_password_x).send_keys('Abcd123$')
     driver.find_element_by_xpath(locators.sign_in_button_x).click()
-    driver.implicitly_wait(20)
+    time.sleep(20)
 
     def fill_and_clear_form(xpath):
         element = driver.find_element_by_xpath(xpath)
@@ -39,19 +40,19 @@ def test_edit_post():
 
         for row in csvreader:
             driver.find_element_by_xpath(locators.user_x).click()
-            driver.implicitly_wait(20)
+            time.sleep(20)
             driver.find_element_by_xpath(locators.first_post_x).click()
-            driver.implicitly_wait(20)
+            time.sleep(20)
             driver.find_element_by_xpath(locators.edit_button_x).click()
-            driver.implicitly_wait(20)
+            time.sleep(20)
             fill_and_clear_form(locators.title_x).send_keys(row[0])
             fill_and_clear_form(locators.about_x).send_keys(row[1])
             fill_and_clear_form(locators.article_x).send_keys(row[2])
             driver.find_element_by_xpath(locators.publish_button_x).click()
-            driver.implicitly_wait(20)
+            time.sleep(20)
 
     driver.find_element_by_xpath(locators.user_x).click()
-    driver.implicitly_wait(20)
+    time.sleep(20)
 
     with open('selenium_tests/edit_data_out.csv', 'w', encoding='UTF-8') as f:
         f.write('Title;About;Article' + '\n')
@@ -63,7 +64,7 @@ def test_edit_post():
         edited_about = edited_abouts_list[l_index].text
 
         edited_titles_list[l_index].click()
-        driver.implicitly_wait(20)
+        time.sleep(20)
 
         edited_article = driver.find_element_by_xpath('//p').text
 
@@ -73,7 +74,7 @@ def test_edit_post():
             file.write(';'.join(edited_data) + '\n')
 
         driver.find_element_by_xpath(locators.user_x).click()
-        driver.implicitly_wait(20)
+        time.sleep(20)
 
     collect_edited_posts_and_write_to_file(-3)
     collect_edited_posts_and_write_to_file(-2)

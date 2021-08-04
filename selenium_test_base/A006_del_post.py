c3c003b41d7dee92bdc6bcd539f542ec263d0e18
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-08-04 15:57:09 +0200

    Test A006 update.

--- selenium_test_base/A006_del_post.py
+++ selenium_test_base/A006_del_post.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import locators
+
 # Headless mode
 opt = Options()
 opt.headless = False
@@ -15,14 +16,13 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), optio
 driver.get(locators.CON_URL)
 time.sleep(2)
 
-
 try:
     # Sign in gombra kattintás
     driver.find_element_by_xpath(locators.sign_in_x).click()
     time.sleep(2)
 
     # Bejelentkezési adatok kitöltése
-    driver.find_element_by_xpath(locators.si_email_x).send_keys('testuser2@example.com')
+    driver.find_element_by_xpath(locators.si_email_x).send_keys('testuser1@example.com')
     driver.find_element_by_xpath(locators.si_password_x).send_keys('Abcd123$')
     driver.find_element_by_xpath(locators.sign_in_button_x).click()
     time.sleep(2)
@@ -38,7 +38,9 @@ try:
 
     # Elem törlésének ellenőrzése:
     driver.find_element_by_xpath(locators.user_x).click()  # Visszanavigálás a saját postok közé
-    title_next_post = driver.find_element_by_xpath(locators.first_post_x)  # Az első elem címének elmentése a törlés után
+
+    # Az első elem címének elmentése a törlés után
+    title_next_post = driver.find_element_by_xpath(locators.first_post_x)
 
     assert (title_post != title_next_post)
 

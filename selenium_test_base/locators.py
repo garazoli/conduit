3c1332023ce08fc149fb6aff8959b170fb4f5172
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-08-04 15:49:56 +0200

    test A000 upload and locators update.

--- selenium_test_base/locators.py
+++ selenium_test_base/locators.py
@@ -2,10 +2,10 @@
 
 CON_URL = 'http://localhost:1667'
 
-# Bejelentkezı - Men˚ gombok:
+# Bejelentkez≈ë - Men√º gombok:
 
-sign_up_x = '//*[@id="app"]/nav/div/ul/li[3]/a'
-sign_in_x = '//*[@id="app"]/nav/div/ul/li[2]/a'
+sign_up_x = '//a[@href="#/register"]'
+sign_in_x = '//a[@href="#/login"]'
 
 # Sign in form:
 
@@ -13,21 +13,23 @@ si_email_x = '//*[@id="app"]/div/div/div/div/form/fieldset[1]/input'
 si_password_x = '//*[@id="app"]/div/div/div/div/form/fieldset[2]/input'
 sign_in_button_x = '//*[@id="app"]/div/div/div/div/form/button'
 
-# Logged in - Men¸ gombok:
+# Logged in - Men√º gombok:
 
 user_x = '//*[@id="app"]/nav/div/ul/li[4]/a'
 new_article_x = '//a[@href="#/editor"]'
 home_x = '//*[@id="app"]/nav/div/ul/li[1]/a'
 log_out_x = "//a/i[@class='ion-android-exit']"
+settings_x = '//a[@href="#/settings"]'
 
-# editor mezık:
+# editor mez≈ëk:
+
 title_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[1]/input'
 about_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[2]/input'
 article_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[3]/textarea'
 tag_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[4]/div/div/ul/li/input'
 publish_button_x = '//*[@id="app"]/div/div/div/div/form/button'
 
-# My posts elsı post
+# My posts els≈ë post title
 
 first_post_x = '//*[@id="app"]/div/div[2]/div/div[1]/div[2]/div/div/div[1]/a/h1'
 
@@ -35,16 +37,30 @@ first_post_x = '//*[@id="app"]/div/div[2]/div/div[1]/div[2]/div/div/div[1]/a/h1'
 
 delete_button_x = '//*[@id="app"]/div/div[1]/div/div/span/button/span'
 edit_button_x = "//a[@class='btn btn-sm btn-outline-secondary']"
+
 # Registration popup:
 
 reg_message_x = '/html/body/div[2]/div/div[2]'
 reg_fail_message_x = '/html/body/div[2]/div/div[3]'
 ok_button_x = '/html/body/div[2]/div/div[4]/div/button'
 
-
-# Sign up mezık:
+# Sign up mez≈ëk:
 
 username_x = '//*[@id="app"]/div/div/div/div/form/fieldset[1]/input'
 email_x = '//*[@id="app"]/div/div/div/div/form/fieldset[2]/input'
 password_x = '//*[@id="app"]/div/div/div/div/form/fieldset[3]/input'
 sign_up_button_x = '//*[@id="app"]/div/div/div/div/form/button'
+
+# Settings oldal mez≈ëi:
+
+new_username_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[2]/input'
+new_email_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[4]/input'
+new_password_x = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[5]/input'
+update_button_x = '//*[@id="app"]/div/div/div/div/form/fieldset/button'
+update_success_x = '/html/body/div[2]/div/div[2]'
+update_ok_button_x = '/html/body/div[2]/div/div[3]/div/button'
+
+# Cookie policy
+
+cookie_panel = 'cookie-policy-panel'
+cookie_accept_btn = '//*[@id="cookie-policy-panel"]/div/div[2]/button[2]'
---
+++ selenium_tests/test_A000_cookie.py
@@ -0,0 +1,35 @@
+# TC_000 teszteset
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import locators
+
+# Headless mode
+opt = Options()
+opt.headless = True
+
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=opt)
+
+# Conduit megnyitása
+driver.get(locators.CON_URL)
+time.sleep(5)
+
+
+def cookies():
+    # A cookie policy panel megjelenésének ellnőrzése:
+    assert driver.find_element_by_id(locators.cookie_panel).is_displayed()
+
+    # Cookie-k elfogadása
+    driver.find_element_by_xpath(locators.cookie_accept_btn).click()
+    time.sleep(5)
+
+    # Újbóli megnyitáskor a cookie elfogdó/elutasító gombok nemlétének ellenőrzése
+    # (csak ez a két gomb van a bejelentkező képernyőn):
+
+    driver.refresh()
+    time.sleep(5)
+
+    buttons = driver.find_elements_by_xpath('//button')
+    assert buttons == []

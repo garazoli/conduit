bf9a8bb5086e1a4ba99b787b6b650b17173d6ab3
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-08-04 16:23:35 +0200

    Test A010 upload.

---
+++ selenium_test_base/A010_edit_password.py
@@ -0,0 +1,72 @@
+# TC_020 teszteset
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import locators
+
+# Headless mode
+opt = Options()
+opt.headless = False
+
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=opt)
+
+# Conduit megnyitasa
+driver.get(locators.CON_URL)
+time.sleep(2)
+
+
+try:
+    # Sign in gombra kattintas
+    driver.find_element_by_xpath(locators.sign_in_x).click()
+    time.sleep(2)
+
+    # Bejelentkezesi adatok kitoltese
+    def sign_in(email, password):
+        driver.find_element_by_xpath(locators.si_email_x).send_keys(email)
+        driver.find_element_by_xpath(locators.si_password_x).send_keys(password)
+        driver.find_element_by_xpath(locators.sign_in_button_x).click()
+        time.sleep(2)
+
+    sign_in('testuser3@example.com', 'Abcd123$')
+
+    # Jelszo megvatoztatasa
+    def edit_settings(password):
+        driver.find_element_by_xpath(locators.settings_x).click()
+        time.sleep(2)
+        driver.find_element_by_xpath(locators.new_password_x).send_keys(password)
+        driver.find_element_by_xpath(locators.update_button_x).click()
+        time.sleep(3)
+
+    edit_settings('Efgh456$')
+
+    assert driver.find_element_by_xpath(locators.update_success_x).text == 'Update successful!'
+
+    driver.find_element_by_xpath(locators.update_ok_button_x).click()
+    time.sleep(3)
+
+    # Logout es bejelentkezes az uj jelszoval
+
+    driver.find_element_by_xpath(locators.log_out_x).click()
+    time.sleep(3)
+
+    driver.find_element_by_xpath(locators.sign_in_x).click()
+    time.sleep(2)
+
+    # Bejelentkezes uj adatokkal
+    sign_in('testuser3@example.com', 'Efgh456$')
+
+    assert driver.find_element_by_xpath(locators.user_x).text == 'testuser3'
+
+    # Eredeti allapot visszaallitasa:
+
+    edit_settings('Abcd123$')
+
+    assert driver.find_element_by_xpath(locators.update_success_x).text == 'Update successful!'
+
+    driver.find_element_by_xpath(locators.update_ok_button_x).click()
+    time.sleep(3)
+
+finally:
+    driver.close()
